79c9ad5b0409d74f1e7aeb5d59a3c66af962b73c
fanjun <fanjun301@qq.com>
2018-02-25 17:45:59 +0800

    refresh config

--- src/main/java/cn/fanstar/server/ServerDemoApplicationConfig.java
+++ src/main/java/cn/fanstar/server/ServerDemoApplicationConfig.java
@@ -1,7 +1,25 @@
 package cn.fanstar.server;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by fanjun on 2018/2/25.
  */
+@Configuration
+@RefreshScope
 public class ServerDemoApplicationConfig {
+
+    @Value("${secret.token}")
+    private String secretToken;
+
+
+    public String getSecretToken() {
+        return secretToken;
+    }
+
+    public void setSecretToken(String secretToken) {
+        this.secretToken = secretToken;
+    }
 }
--- src/main/java/cn/fanstar/server/controller/TestController.java
+++ src/main/java/cn/fanstar/server/controller/TestController.java
@@ -1,5 +1,7 @@
 package cn.fanstar.server.controller;
 
+import cn.fanstar.server.ServerDemoApplicationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/")
 public class TestController {
 
+    @Autowired
+    private ServerDemoApplicationConfig serverDemoApplicationConfig;
+
 
     @GetMapping("/hello")
     public String hello(){
 
         return "greeting from server-demo";
+    }
+
+    @GetMapping("/token")
+    public String token(){
+
+        return String.format(" token is : %s", serverDemoApplicationConfig.getSecretToken() );
     }
 
 }
